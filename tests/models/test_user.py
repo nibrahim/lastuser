0c7e4cf960835738d57d7e0844cbed41c69f12cf
Mitesh Ashar <email@miteshashar.com>
2013-08-20 19:21:47 +0530

    Starting to create tests for User model. Also started conceptualising & working on integrated object bootstrapping functions in the fixture base class.

---
+++ tests/models/test_user.py
@@ -0,0 +1,17 @@
+import unittest
+from sqlalchemy.exc import IntegrityError
+from lastuser_core.models import User
+from ..test_db import db, BasicTestFixture
+
+class TestUserModel(BasicTestFixture):
+    def setUp(self):
+        super(TestUserModel, self).setUp(5)
+        # self.user = User.query.filter_by()
+        print self.users
+
+
+    def test_noop(self):
+        pass
+
+    def tearDown(setUp):
+        pass
--- tests/test_db.py
+++ tests/test_db.py
@@ -2,17 +2,32 @@
 
 import unittest
 from lastuserapp import app, db, init_for
+from lastuser_core.models import *
 from .fixtures import make_fixtures
 
-class TestDatabaseFixture(unittest.TestCase):
-    def setUp(self):
+class BasicTestFixture(unittest.TestCase):
+    def setUp(self, users=1, clients=1, orgs=1, user_clients=0, org_clients=0):
         init_for('testing')
         app.config['TESTING'] = True
         db.app = app
         db.drop_all()
         db.create_all()
         self.db = db
-        make_fixtures()
+        self.bootstrap_users(users)
+        # make_fixtures()
+
+    def bootstrap_users(self, n=2):
+        if hasattr(self, 'users') is False:
+            self.users = []
+        if n - len(self.users) >= 0:
+            for i in range(n - len(self.users)):
+                self.users.append(
+                    User(
+                        username=u"user" + unicode(i),
+                        fullname=u"User " + unicode(i)
+                        )
+                    )
+
 
     def tearDown(self):
         self.db.drop_all()
