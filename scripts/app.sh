249aa13cf4fc5ca22fe32719829af80893f126f9
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-09-23 16:54:43 +0530

    Fixes port and some minor details

--- scripts/app.sh
+++ scripts/app.sh
@@ -3,8 +3,8 @@
 # Management script for Lastuser instance Managed by system monit. 
 
 # If you change any of these file locations, please make corresponding
-# changes in the /etc/monit/conf.d/frp.conf file on the server. The
-# local file scripts/monit/frp.conf is what you should change and
+# changes in the /etc/monit/conf.d/lastuser.conf file on the server. The
+# local file scripts/monit/lastuser.conf is what you should change and
 # deploy if you want to do this.
 
 VENV=/opt/frp/environments/lastuser
@@ -12,16 +12,17 @@ APP_ROOT=/opt/lastuser/deployed
 PID_FILE=/opt/lastuser/lastuser.pid
 ACCESS_LOG_FILE=/var/log/frp/lastuser-gunicorn.access.log
 ERROR_LOG_FILE=/var/log/frp/lastuser-gunicorn.error.log
+BIND_ADDRESS="localhost:8001"
 
-FRP_CONFIG=production
 
 # Activate virtualenv
 . ${VENV}/bin/activate
 
 case $1 in
    start)
-      gunicorn --chdir ${APP_ROOT}/frp \
+      gunicorn --chdir ${APP_ROOT} \
 	  -D \
+	  -b ${BIND_ADDRESS} \
 	  -p ${PID_FILE} \
 	  --access-logfile=${ACCESS_LOG_FILE} \
 	  --error-logfile=${ERROR_LOG_FILE} \
